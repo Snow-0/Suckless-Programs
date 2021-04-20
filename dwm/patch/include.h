d1462969c0c2ab856f3d2a0ca5797cf151ac39af
Snow-0 <69723902+Snow-0@users.noreply.github.com>
2021-04-20 08:56:30 -0400

    Add files via upload

---
+++ dwm/patch/include.h
@@ -0,0 +1,25 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_dwmblocks.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_statuscmd.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "autostart.h"
+#include "cyclelayouts.h"
+#include "movestack.h"
+#include "pertag.h"
+#include "scratchpad.h"
+#include "swallow.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_gapplessgrid.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
